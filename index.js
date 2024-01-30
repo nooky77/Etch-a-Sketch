e0b9288ff01b9bd696a952595035fbd658165af8
Youcef Sabri <rsg951@gmail.com>
2024-01-30 03:34:58 +0100

    add reset grid function

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const grid = document.querySelector(".grid");
+const resetBtn = document.querySelector("#reset-btn");
 let gridSize = 16;
-let doubleSize = gridSize * gridSize;
 
-for (let i = 0; i < doubleSize; i++) {
-	let box = document.createElement("div");
-	box.className = "box";
-	box.style.width = `${800 / gridSize}px`;
-	box.style.height = `${800 / gridSize}px`;
-	grid.appendChild(box);
-}
+printGrid(gridSize);
 
 grid.addEventListener("mouseover", (e) => {
 	let targetedBox = e.target;
 	targetedBox.style.backgroundColor = getRgbColor();
 });
+
+resetBtn.addEventListener("click", resetGrid);
+
+function printGrid(size) {
+	let doubleSize = size * size;
+	for (let i = 0; i < doubleSize; i++) {
+		let box = document.createElement("div");
+		box.className = "box";
+		box.style.width = `${800 / gridSize}px`;
+		box.style.height = `${800 / gridSize}px`;
+		grid.appendChild(box);
+	}
+}
+
+function resetGrid() {
+	const boxes = document.querySelectorAll(".box");
+	[...boxes].forEach((box) => (box.style.backgroundColor = "black"));
+}
 
 function getRgbColor() {
 	let arrColor = [];
