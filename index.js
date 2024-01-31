97b798f61bfcb32f04e46d90957633f3a38082c1
Youcef Sabri <rsg951@gmail.com>
2024-01-31 02:11:10 +0100

    cleanup code

--- index.js
+++ index.js
@@ -2,18 +2,12 @@ const grid = document.querySelector(".grid");
 const gridContainer = document.querySelector(".grid-container");
 const gridSizeBtn = document.querySelector("#grid-size-btn");
 const resetBtn = document.querySelector("#reset-btn");
-let gridSize = 16;
 
-printGrid(gridSize);
-
-grid.addEventListener("mouseover", (e) => {
-	let targetedBox = e.target;
-	targetedBox.style.backgroundColor = getRgbColor();
-});
+grid.addEventListener("mouseover", (e) => printBox(e));
 
 gridSizeBtn.addEventListener("click", resizeGrid);
 
-resetBtn.addEventListener("click", resetGrid);
+resetBtn.addEventListener("click", resetColor);
 
 function printGrid(size) {
 	let doubleSize = size * size;
@@ -26,6 +20,20 @@ function printGrid(size) {
 	}
 }
 
+function randomColor() {
+	let arrColor = [];
+	for (let i = 0; i < 3; i++) {
+		arrColor.push(Math.floor(Math.random() * 256));
+	}
+	let color = `rgb(${arrColor[0]}, ${arrColor[1]}, ${arrColor[2]})`;
+	return color;
+}
+
+function printBox(e, color = randomColor()) {
+	let targetedBox = e.target;
+	targetedBox.style.backgroundColor = color;
+}
+
 function resizeGrid() {
 	let newSize = +prompt("Enter a number, max: 100");
 	if (typeof newSize !== "number" || newSize < 2) return;
@@ -34,16 +42,9 @@ function resizeGrid() {
 	printGrid(newSize);
 }
 
-function resetGrid() {
+function resetColor() {
 	const boxes = document.querySelectorAll(".box");
-	[...boxes].forEach((box) => (box.style.backgroundColor = "black"));
+	[...boxes].forEach((box) => (box.style.backgroundColor = "#252525"));
 }
 
-function getRgbColor() {
-	let arrColor = [];
-	for (let i = 0; i < 3; i++) {
-		arrColor.push(Math.floor(Math.random() * 256));
-	}
-	let color = `rgb(${arrColor[0]}, ${arrColor[1]}, ${arrColor[2]})`;
-	return color;
-}
+printGrid(16);
